39f08adc50ea35fff56d959f860a059edea9261e
Richard Niemand <niemand.richard@gmail.com>
2021-07-12 17:17:31 -0600

    Updating build

--- ci-test.ps1
+++ ci-test.ps1
@@ -1,44 +1,43 @@
-$cfgSolutionFile    = "HassUtils.sln";
-$cfgConfiguration   = "Release";
+param (
+  $outputRoot = $PSScriptRoot
+)
 
- 
-# ===============================================================================
-# region :: Setup Directories
-# ===============================================================================
-$workingDir            = $PSScriptRoot;
-$PublishDirectoryName  = "artifacts";
+$workingDir            = Join-Path $PSScriptRoot "\";
+$outputRoot            = Join-Path $outputRoot "\";
 $sourceDir             = Join-Path $workingDir "src";
+$publishDir            = Join-Path $outputRoot "artifacts";
 $toolsDir              = Join-Path $workingDir "tools";
-$slnFilePath           = Join-Path $sourceDir $cfgSolutionFile
-$publishDir            = Join-Path $workingDir $PublishDirectoryName
-$T1TestDir             = Join-Path $publishDir "t1-test";
-$coverageDirectory     = Join-Path $T1TestDir "coverage-report"
-$coverageDirectoryHtml = Join-Path $coverageDirectory "html"
-$ProjectIncludePattern = "*T1.Tests.csproj";
-$coverage              = Join-Path $workingDir "/src/HassUtils.T1.Tests/bin/Release/net5.0/HassUtils.T1.Tests.dll"
+$testPublishDir        = Join-Path $publishDir "t1-publih";
+$testResultsDir        = Join-Path $publishDir "t1-results";
+$testCoverageDir       = Join-Path $publishDir "t1-coverage";
+$coverletExe           = "$toolsDir\coverlet.exe";
+$reportGeneratorExe    = "$toolsDir\reportgenerator.exe"
+$testCsprojPattern     = "*T1.Tests.csproj";
+$buildConfiguration    = "Release";
+$installCmd            = "";
+$buildCmd              = "";
+$currentPublishDir     = "";
 
 
-# ===============================================================================
-# region :: Cleanup
-# ===============================================================================
+# ==============================================================
+# Cleanup
+# ==============================================================
+#
 $cleanupDirectories = @();
-$cleanupDirectories += $publishDir;
-$cleanupDirectories += $toolsDir;
+$cleanupDirectories += $testPublishDir;
+$cleanupDirectories += $testCoverageDir;
+$cleanupDirectories += $testResultsDir;
  
-Foreach ($cleanupDirectory in $cleanupDirectories) {
+foreach ($cleanupDirectory in $cleanupDirectories) {
   if (Test-Path $cleanupDirectory) {
     Remove-item $cleanupDirectory -Recurse -Force | Out-Null;
   }
 }
 
-
-# ===============================================================================
-# region :: Required tooling
-# ===============================================================================
-$installCmd          = "";
-$coverletExe         = "$toolsDir\coverlet.exe";
-$reportGeneratorExe  = "$toolsDir\reportgenerator.exe"
-
+# ==============================================================
+# Install tooling
+# ==============================================================
+#
 if(!(Test-Path $coverletExe)){
   Write-Host "Installing: coverlet.console"
   $installCmd = "dotnet tool install coverlet.console --tool-path $toolsDir";
@@ -51,42 +50,53 @@ if(!(Test-Path $reportGeneratorExe)){
   Invoke-Expression -Command $installCmd -ErrorAction 'Continue';
 }
 
-
-# ===============================================================================
-# region :: Discover and run tests
-# ===============================================================================
-$testProjects = Get-ChildItem -Path $sourceDir -Include $ProjectIncludePattern -Recurse -File
+# ==============================================================
+# Discover and Build test projects
+# ==============================================================
+#
+$testProjects = Get-ChildItem -Path $sourceDir -Include $testCsprojPattern -Recurse -File
 
 if ($testProjects.count -eq 0) {
-  throw "No files matched the $ProjectIncludePattern pattern. The script cannot continue."
+  throw "No files matched the $testCsprojPattern pattern. The script cannot continue."
 }
 
 foreach ($testProject in $testProjects) {
-  $testResultFileName = Join-Path $T1TestDir "$( $testProject.BaseName )_results.trx"
-  $coverageReportOutputPath = Join-Path $coverageDirectory "\"
- 
-  # The arguments passed to the `dotnet test` command
-  # Reference: https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-test
+  # Build test project
+  $dllFileName = $testProject.BaseName + ".dll";
+  $currentPublishDir = Join-Path $testPublishDir $testProject.BaseName
+  $testDllFile = Join-Path $currentPublishDir $dllFileName;
+  $buildCmd = "dotnet build `"$testProject`" --configuration $buildConfiguration --output `"$currentPublishDir`"";
+  Invoke-Expression $buildCmd;
+
+  if(!(Test-Path $testDllFile)) {
+    throw "Unable to find test DLL file: $testDllFile"
+  }
+  
+  # Generate coverage report
+  $testResultFileName   = Join-Path $testResultsDir "$( $testProject.BaseName )_results.trx";
+  $coverageOutputDirTmp = Join-Path $testCoverageDir $testProject.BaseName;
+  $coverageOutputDir    = Join-Path $coverageOutputDirTmp "\";
+
+  # https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-test
   $dotnetTestTargetArgs = @(
     "test",
     "$( $testProject.FullName )",
     "--logger:trx;LogFileName=$testResultFileName",
-    "--configuration $cfgConfiguration",
+    "--configuration $buildConfiguration",
     "--no-build", 
-    "--no-restore";
-  )
- 
-  # The arguments passed to Coverlet
-  # Reference: https://www.jetbrains.com/help/dotcover/dotCover__Console_Runner_Commands.html
+    "--no-restore"
+  );
+  
+  # https://www.jetbrains.com/help/dotcover/dotCover__Console_Runner_Commands.html
   $coverletDotnetArguments = @(
-    $coverage,
+    $testDllFile,
     "-t `"dotnet`"",
     "-a `"$dotnetTestTargetArgs`"",
-    "-o $coverageReportOutputPath",
+    "-o $coverageOutputDir",
     "-f `"cobertura`"",
     "-f `"opencover`""
-  )
- 
+  );
+
   $coverletCmd = "$coverletExe $coverletDotnetArguments";
   Write-Output "Executing Coverlet with following arguments: $coverletCmd";
   Invoke-Expression -Command $coverletCmd;
