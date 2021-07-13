1aefe7a0b6409ad090a5b7ad6d68e1d6c89abb81
Richard Niemand <niemand.richard@gmail.com>
2021-07-12 18:24:45 -0600

    Working on configuration

---
+++ src/HassUtils.Shared/Configuration/HassUtilsConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace HassUtils.Shared.Configuration
+{
+  public class HassUtilsConfig
+  {
+    [JsonProperty("Server"), JsonPropertyName("Server")]
+    public HomeAssistantServerConfig Server { get; set; }
+
+    public HassUtilsConfig()
+    {
+      Server = new HomeAssistantServerConfig();
+    }
+  }
+}
---
+++ src/HassUtils.T1.Tests/Configuration/HassUtilsConfigTests.cs
@@ -0,0 +1,20 @@
+﻿using HassUtils.Shared.Configuration;
+using NUnit.Framework;
+
+namespace HassUtils.T1.Tests.Configuration
+{
+  [TestFixture]
+  public class HassUtilsConfigTests
+  {
+    [Test]
+    public void HassUtilsConfig_Given_Constructed_ShouldDefault_Server()
+    {
+      // arrange
+      var config = new HassUtilsConfig();
+
+      // assert
+      Assert.IsNotNull(config.Server);
+      Assert.IsInstanceOf<HomeAssistantServerConfig>(config.Server);
+    }
+  }
+}
